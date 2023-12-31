1c09e85876b565543a6fac9103b2cf41febb465e
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2023-12-31 02:01:45 +0100

    Faster uploading
    updating drivers and improving code

--- main.py
+++ main.py
@@ -1,86 +1,73 @@
-import time, os
+import time
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.ChromeOptions()
-# options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument("--log-level=3")
-options.add_argument("user-data-dir=C:\\Users\\User\\AppData\\Local\Google\\Chrome Beta\\User Data\\")
-options.binary_location = "C:\\Program Files\\Google\\Chrome Beta\\Application\\chrome.exe"
-print("\033[1;31;40m IMPORTANT: Put one or more videos in the *videos* folder in the bot directory. Please make sure to name the video files like this --> Ex: vid1.mp4 vid2.mp4 vid3.mp4 etc..")
-time.sleep(6)
-answer = input("\033[1;32;40m Press 1 if you want to spam same video or Press 2 if you want to upload multiple videos: ")
+options.add_argument("user-data-dir=C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data\\")
+options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 
-if(int(answer) == 1):
-    nameofvid = input("\033[1;33;40m Put the name of the video you want to upload (Ex: vid.mp4 or myshort.mp4 etc..) ---> ")
-    howmany = input("\033[1;33;40m How many times you want to upload this video ---> ")
 
-    for i in range(int(howmany)):
-        bot = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
+def wait_and_click(driver, xpath):
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+    element.click()
 
-        bot.get("https://studio.youtube.com")
-        time.sleep(3)
-        upload_button = bot.find_element(By.XPATH, '//*[@id="upload-icon"]')
-        upload_button.click()
-        time.sleep(1)
 
-        file_input = bot.find_element(By.XPATH, '//*[@id="content"]/input')
-        simp_path = 'videos/{}'.format(str(nameofvid))
-        abs_path = os.path.abspath(simp_path)
-        file_input.send_keys(abs_path)
+def upload_video(video_path):
+    bot = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
+    bot.get("https://studio.youtube.com")
 
-        time.sleep(7)
+    wait_and_click(bot, '//*[@id="upload-icon"]')
 
-        next_button = bot.find_element(By.XPATH, '//*[@id="next-button"]')
-        for i in range(3):
-            next_button.click()
-            time.sleep(1)
+    file_input = bot.find_element(By.XPATH, '//*[@id="content"]/input')
+    file_input.send_keys(video_path)
 
-        done_button = bot.find_element(By.XPATH, '//*[@id="done-button"]')
-        done_button.click()
-        time.sleep(5)
-        bot.quit()
+    for _ in range(3):
+        wait_and_click(bot, '//*[@id="next-button"]')
 
-elif(int(answer) == 2):
-    print("\033[1;31;40m IMPORTANT: Please make sure the name of the videos are like this: vid1.mp4, vid2.mp4, vid3.mp4 ...  etc")
-    dir_path = '.\\videos'
-    count = 0
+    wait_and_click(bot, '//*[@name="VIDEO_MADE_FOR_KIDS_NOT_MFK"]')
+    for _ in range(3):
+        wait_and_click(bot, '//*[@id="next-button"]')
 
-    for path in os.listdir(dir_path):
-        if os.path.isfile(os.path.join(dir_path, path)):
-            count += 1
-    print("   ", count, " Videos found in the videos folder, ready to upload...")
+    wait_and_click(bot, '//*[@name="PUBLIC"]')
+    wait_and_click(bot, '//*[@id="next-button"]')
+
+    for _ in range(3):
+        wait_and_click(bot, '//*[@id="done-button"]')
+
+    with bot:
+        pass
+
+def main():
+    print("\033[1;31;40m IMPORTANT: Put one or more videos in the *videos* folder in the bot directory. Please make sure to name the video files like this --> Ex: vid1.mp4 vid2.mp4 vid3.mp4 etc..")
     time.sleep(6)
+    
+    # answer = input("\033[1;32;40m Press 1 if you want to spam the same video or Press 2 if you want to upload multiple videos: ")
+    answer = 2
 
-    for i in range(count):
-        bot = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
+    if int(answer) == 1:
+        nameofvid = input("\033[1;33;40m Put the name of the video you want to upload (Ex: vid.mp4 or myshort.mp4 etc..) ---> ")
+        howmany = input("\033[1;33;40m How many times you want to upload this video ---> ")
 
-        bot.get("https://studio.youtube.com")
-        time.sleep(3)
-        upload_button = bot.find_element(By.XPATH, '//*[@id="upload-icon"]')
-        upload_button.click()
-        time.sleep(1)
+        for i in range(int(howmany)):
+            video_path = os.path.abspath(os.path.join('videos', nameofvid))
+            upload_video(video_path)
 
-        file_input = bot.find_element(By.XPATH, '//*[@id="content"]/input')
-        simp_path = 'videos/vid{}.mp4'.format(str(i+1))
-        abs_path = os.path.abspath(simp_path)
-        
-        file_input.send_keys(abs_path)
+    elif int(answer) == 2:
+        print("\033[1;31;40m IMPORTANT: Please make sure the name of the videos is like this: vid1.mp4, vid2.mp4, vid3.mp4 ...  etc")
+        dir_path = '.\\videos'
+        count = len([path for path in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, path))])
 
-        time.sleep(7)
+        print("   ", count, " Videos found in the videos folder, ready to upload...")
+        time.sleep(6)
 
-        next_button = bot.find_element(By.XPATH, '//*[@id="next-button"]')
-        for i in range(3):
-            next_button.click()
-            time.sleep(1)
+        for i in range(count):
+            video_path = os.path.abspath(os.path.join('videos', 'vid{}.mp4'.format(i+1)))
+            upload_video(video_path)
 
-        done_button = bot.find_element(By.XPATH, '//*[@id="done-button"]')
-        done_button.click()
-        time.sleep(5)
-        bot.quit()
-
-
-
-
+if __name__ == "__main__":
+    main()
